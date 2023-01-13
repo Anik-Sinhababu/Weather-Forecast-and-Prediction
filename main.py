c5bce133c1abe9e33c67b974a06cc1ccd2b8fff9
ANIK <sinhababuanik@gmail.com>
2023-01-13 14:47:24 +0530

    wehave commited the GUI of the weather app and plotted some fake data to see the interaction and the functioning of the app

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import plotly.express as px
+
+import base64
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True
+    )
+
+
+add_bg_from_local('img_1.png')
+
+st.title("Weather Forecast for the next days")
+
+place = st.text_input("place: ")
+
+days = st.slider("Forecast Days", min_value=1, max_value=5, help="select the number of forecasted days")
+
+option = st.selectbox("select data to view", ("Temperature", "Sky"))
+
+st.subheader(f"{option} for the next {days} days in {place}")
+
+
+def get_data(days):
+    date = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [10, 11, 15]
+    temperatures = [days * i for i in temperatures]
+    return date, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
