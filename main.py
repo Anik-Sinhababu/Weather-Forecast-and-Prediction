d0a6a1d87abfacf8f5029958ea6ba6d2724362d2
ANIK <sinhababuanik@gmail.com>
2023-01-13 16:21:23 +0530

    we have committed and filtered the weather json file and taken the required information

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
+
 
 import base64
 
@@ -32,13 +34,7 @@ option = st.selectbox("select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    date = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return date, temperatures
-
+data = get_data(place, forecast_days, kind)
 d, t = get_data(days)
 
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
